163016a9f437f1956e6463097b4aecba54a31eb2
Frost Ming <me@frostming.com>
2023-12-24 11:03:02 +0800

    feat: add some tests
    Signed-off-by: Frost Ming <me@frostming.com>

--- backend/bamboo/database/models.py
+++ backend/bamboo/database/models.py
@@ -1,28 +1,36 @@
 from datetime import datetime
-from typing import Optional
+from typing import NoReturn, Optional
 
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from werkzeug.security import check_password_hash, generate_password_hash
-
-from bamboo.utils import utc_now
 
 db = SQLAlchemy()
 
 
-class User(db.Model):
+class Base(db.Model):
+    __abstract__ = True
+
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    created_at: so.Mapped[datetime] = so.mapped_column(default=func.now())
+    updated_at: so.Mapped[datetime] = so.mapped_column(default=func.now(), onupdate=func.now())
+
+    def __repr__(self):
+        return f"<{self.__class__.__name__} id={self.id}>"
+
+
+class User(Base):
     name: so.Mapped[str]
-    username: so.Mapped[Optional[str]]
+    username: so.Mapped[Optional[str]] = so.mapped_column(unique=True, index=True)
     password_hash: so.Mapped[Optional[str]]
     bio: so.Mapped[Optional[str]]
     introduction: so.Mapped[Optional[str]]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     active: so.Mapped[bool] = so.mapped_column(default=False)
+    is_superuser: so.Mapped[bool] = so.mapped_column(default=False)
     profile_image_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("media.id"), index=True)
-    role_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("role.id"), index=True)
+    role_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("role.id"), index=True)
     profile_image: so.Mapped["Media"] = so.relationship(foreign_keys=[profile_image_id])
     role: so.Mapped["Role"] = so.relationship(back_populates="users")
     blog_authors: so.WriteOnlyMapped["BlogAuthor"] = so.relationship(
@@ -30,43 +38,35 @@ class User(db.Model):
     )
 
     @property
-    def password(self):
+    def password(self) -> NoReturn:
         raise AttributeError("Write-only property!")
 
     @password.setter
-    def password(self, password):
+    def password(self, password: str) -> None:
         self.password_hash = generate_password_hash(password)
 
-    def validate_password(self, password):
+    def validate_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
 
 
-class Role(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Role(Base):
     name: so.Mapped[str]
     permissions: so.Mapped[int]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     users: so.WriteOnlyMapped["User"] = so.relationship(back_populates="role")
 
 
-class Media(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Media(Base):
     path: so.Mapped[str]
     content_type: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
 
 
-class Site(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Site(Base):
+    name: so.Mapped[str]
     config: so.Mapped[dict] = so.mapped_column(type_=sa.JSON)
     template_url: so.Mapped[Optional[str]]
     deploy_target: so.Mapped[Optional[str]]
     deploy_method: so.Mapped[Optional[str]]
     deploy_secret: so.Mapped[Optional[str]] = so.mapped_column(sa.Text)
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     pages: so.WriteOnlyMapped["Page"] = so.relationship(back_populates="site")
     notifications: so.WriteOnlyMapped["Notification"] = so.relationship(back_populates="site")
     volunteer_forms: so.WriteOnlyMapped["VolunteerForm"] = so.relationship(back_populates="site")
@@ -74,95 +74,82 @@ class Site(db.Model):
     speaker_forms: so.WriteOnlyMapped["SpeakerForm"] = so.relationship(back_populates="site")
     talks: so.WriteOnlyMapped["Talk"] = so.relationship(back_populates="site")
     blogs: so.WriteOnlyMapped["Blog"] = so.relationship(back_populates="site")
+    cities: so.WriteOnlyMapped["City"] = so.relationship(back_populates="site")
 
 
-class Page(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Page(Base):
     title: so.Mapped[str]
     path: so.Mapped[str]
     content: so.Mapped[str] = so.mapped_column(sa.Text)
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="pages")
 
 
-class Notification(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Notification(Base):
     content: so.Mapped[str] = so.mapped_column(sa.Text)
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="notifications")
 
 
-class VolunteerForm(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class VolunteerForm(Base):
     # ...
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="volunteer_forms")
 
 
-class SponsorForm(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class SponsorForm(Base):
     # ...
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="sponsor_forms")
 
 
-class SpeakerForm(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class SpeakerForm(Base):
     # ...
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="speaker_forms")
 
 
-class Talk(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+talk_category = db.Table(
+    "talk_category",
+    sa.Column(
+        "talk_id", sa.Integer, sa.ForeignKey("talk.id", ondelete="CASCADE"), primary_key=True
+    ),
+    sa.Column(
+        "category_id",
+        sa.Integer,
+        sa.ForeignKey("category.id", ondelete="CASCADE"),
+        primary_key=True,
+    ),
+)
+
+
+class Talk(Base):
     title: so.Mapped[str]
-    content: so.Mapped[str]
+    content: so.Mapped[str] = so.mapped_column(sa.Text, default="")
     video_url: so.Mapped[Optional[str]]
     slides_url: so.Mapped[Optional[str]]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     slides_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("media.id"), index=True)
     slides: so.Mapped["Media"] = so.relationship(foreign_keys=[slides_id])
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="talks")
-    schedule_items: so.WriteOnlyMapped["ScheduleItem"] = so.relationship(
+    schedule_item: so.Mapped[Optional["ScheduleItem"]] = so.relationship(
         back_populates="talk", uselist=False, cascade="all, delete-orphan"
     )
-    talk_categories: so.WriteOnlyMapped["TalkCategory"] = so.relationship(
-        back_populates="talk", cascade="all, delete-orphan"
+    categories: so.WriteOnlyMapped["Category"] = so.relationship(
+        secondary=talk_category, back_populates="talks", passive_deletes=True
     )
 
 
-class Category(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Category(Base):
     name: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
-    talk_categories: so.WriteOnlyMapped["TalkCategory"] = so.relationship(
-        back_populates="category", cascade="all, delete-orphan"
+    talks: so.WriteOnlyMapped["Talk"] = so.relationship(
+        secondary=talk_category, passive_deletes=True
     )
-
-
-class TalkCategory(db.Model):
-    talk_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("talk.id"), primary_key=True)
-    category_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("category.id"), primary_key=True)
-    talk: so.Mapped["Talk"] = so.relationship(back_populates="talk_categories")
-    category: so.Mapped["Category"] = so.relationship(back_populates="talk_categories")
 
 
 class Staff(db.Model):
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
+    created_at: so.Mapped[datetime] = so.mapped_column(default=func.now())
+    updated_at: so.Mapped[datetime] = so.mapped_column(default=func.now(), onupdate=func.now())
     city_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("city.id"), primary_key=True)
     staff_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("user.id"), primary_key=True)
     city: so.Mapped["City"] = so.relationship(back_populates="staffs")
@@ -170,19 +157,17 @@ class Staff(db.Model):
     category: so.Mapped[str]
 
 
-class City(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    name: so.Mapped[str]
-    address: so.Mapped[str]
-    latitude: so.Mapped[float]
-    longitude: so.Mapped[float]
-    start: so.Mapped[datetime]
-    end: so.Mapped[datetime]
-    registration_url: so.Mapped[str]
-    live_urls: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
+class City(Base):
+    name: so.Mapped[str] = so.mapped_column(index=True)
+    address: so.Mapped[Optional[str]]
+    latitude: so.Mapped[Optional[str]]
+    longitude: so.Mapped[Optional[str]]
+    start: so.Mapped[Optional[datetime]]
+    end: so.Mapped[Optional[datetime]]
+    registration_url: so.Mapped[Optional[str]]
+    live_urls: so.Mapped[Optional[str]]
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
+    site: so.Mapped["Site"] = so.relationship(back_populates="cities")
     staffs: so.WriteOnlyMapped["Staff"] = so.relationship(back_populates="city")
     venues: so.WriteOnlyMapped["Venue"] = so.relationship(back_populates="city")
     partnerships: so.WriteOnlyMapped["Partership"] = so.relationship(
@@ -196,18 +181,15 @@ class Partership(db.Model):
         sa.ForeignKey("organization.id"), primary_key=True
     )
     category: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
+    created_at: so.Mapped[datetime] = so.mapped_column(default=func.now())
+    updated_at: so.Mapped[datetime] = so.mapped_column(default=func.now(), onupdate=func.now())
     city: so.Mapped["City"] = so.relationship(back_populates="partnerships")
     organization: so.Mapped["Organization"] = so.relationship(back_populates="partnerships")
 
 
-class Organization(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Organization(Base):
     name: so.Mapped[str]
     url: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     profile_image_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("media.id"), index=True)
     profile_image: so.Mapped["Media"] = so.relationship(foreign_keys=[profile_image_id])
     partnerships: so.WriteOnlyMapped["Partership"] = so.relationship(
@@ -215,13 +197,10 @@ class Organization(db.Model):
     )
 
 
-class Blog(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Blog(Base):
     title: so.Mapped[str]
     path: so.Mapped[str]
     content: so.Mapped[str] = so.mapped_column(sa.Text)
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     site_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("site.id"), index=True)
     site: so.Mapped["Site"] = so.relationship(back_populates="blogs")
     blog_authors: so.WriteOnlyMapped["BlogAuthor"] = so.relationship(
@@ -236,24 +215,19 @@ class BlogAuthor(db.Model):
     author: so.Mapped["User"] = so.relationship(back_populates="blog_authors")
 
 
-class ScheduleItem(db.Model):
-    venue_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("venue.id"), primary_key=True)
-    talk_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("talk.id"), primary_key=True)
-    content: so.Mapped[str] = so.mapped_column(sa.Text)
+class ScheduleItem(Base):
+    venue_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("venue.id"), index=True)
+    talk_id: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("talk.id"), index=True)
+    content: so.Mapped[str] = so.mapped_column(sa.Text, default="")
     start: so.Mapped[datetime]
     end: so.Mapped[datetime]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     venue: so.Mapped["Venue"] = so.relationship(back_populates="schedule_items")
-    talk: so.Mapped["Talk"] = so.relationship(back_populates="schedule_items")
+    talk: so.Mapped["Talk"] = so.relationship(back_populates="schedule_item", uselist=False)
 
 
-class Venue(db.Model):
-    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+class Venue(Base):
     name: so.Mapped[str]
     address: so.Mapped[str]
-    created_at: so.Mapped[datetime] = so.mapped_column(default=utc_now)
-    updated_at: so.Mapped[datetime] = so.mapped_column(default=utc_now, onupdate=utc_now)
     city_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey("city.id"), index=True)
     city: so.Mapped["City"] = so.relationship(back_populates="venues")
     schedule_items: so.WriteOnlyMapped["ScheduleItem"] = so.relationship(

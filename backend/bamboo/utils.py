8481217b809ceda9bcab98fc4da7e14ddc10d74d
uncle-lv <uncle.lv@outlook.com>
2023-12-25 13:11:13 +0000

    feat(utils.py): JWT utils

--- backend/bamboo/utils.py
+++ backend/bamboo/utils.py
@@ -1,6 +1,101 @@
-from datetime import UTC, datetime
+import uuid
+from datetime import UTC, datetime, timedelta
+from typing import (
+    Any,
+    Container,
+    Iterable,
+    MutableMapping,
+)
+
+from jose import jwt
 
 
 def utc_now() -> datetime:
     """Get the current time in UTC."""
     return datetime.now(UTC)
+
+
+def _encode_jwt(
+    extra_claims: MutableMapping[str, Any],
+    secret_key: str,
+    token_type: str = "access",
+    expires_delta: timedelta = timedelta(minutes=30),
+    algorithm: str = "HS256",
+    issuer: str | None = None,
+    subject: str | None = None,
+    audience: str | Iterable[str] | None = None,
+    nbf: bool = False,
+    jti: bool = False,
+) -> str:
+    """Create a JWT.
+
+    Args:
+        extra_claims: _description_
+        secret_key: The secret key for JWT.
+        token_type: The type of JWT.("access" or "refresh")
+        expires_delta: The duration of JWT.
+        algorithm: The signing algorithm being used.
+        others: For details about other args, see [the JWT RFC](https://datatracker.ietf.org/doc/html/rfc7519)
+
+    Returns:
+        str: Encoded token
+    """
+    now = utc_now()
+
+    claims = {
+        "type": token_type,
+        "iat": now,
+        "exp": now + expires_delta,
+    }
+
+    if issuer:
+        claims["iss"] = issuer
+
+    if subject:
+        claims["sub"] = subject
+
+    if audience:
+        claims["aud"] = audience
+
+    if nbf:
+        claims["nbf"] = now
+
+    if jti:
+        claims["jti"] = str(uuid.uuid4())
+
+    if extra_claims:
+        claims.update(extra_claims)
+
+    return jwt.encode(
+        claims=claims,
+        key=secret_key,
+        algorithm=algorithm,
+    )
+
+
+def _decode_jwt(
+    encoded_token: str,
+    secret_key: str,
+    algorithms: str | Container[str] = "HS256",
+    issuer: str | None = None,
+    audience: str | Iterable[str] | None = None,
+) -> dict[str, Any]:
+    """Verify and decode JWT.
+
+    Args:
+        encoded_token: The JWT to verify and decode.
+        secret_key: The secret key for JWT.
+        algorithm: The signing algorithm being used.
+        issuer (str | None, optional): _description_. Defaults to None.
+        others: For details about other args, see [the JWT RFC](https://datatracker.ietf.org/doc/html/rfc7519)
+
+    Returns:
+        dict[str, Any]: _description_
+    """
+    return jwt.decode(
+        token=encoded_token,
+        key=secret_key,
+        algorithms=algorithms,
+        issuer=issuer,
+        audience=audience,
+    )
---
+++ backend/tests/test_utils.py
@@ -0,0 +1,76 @@
+from datetime import timedelta
+
+import pytest
+from bamboo.utils import _decode_jwt, _encode_jwt
+from jose.exceptions import ExpiredSignatureError, JWTClaimsError, JWTError
+
+
+def test_simple_jwt():
+    encoded_token = _encode_jwt(
+        extra_claims={"username": "bamboo"},
+        secret_key="bamboo",
+    )
+
+    decoded_token = _decode_jwt(encoded_token=encoded_token, secret_key="bamboo")
+
+    assert decoded_token["username"] == "bamboo"
+    assert decoded_token["type"] == "access"
+    assert decoded_token["iat"] is not None
+    assert decoded_token["exp"] == (decoded_token["iat"] + timedelta(minutes=30).total_seconds())
+    assert decoded_token.get("iss") is None
+    assert decoded_token.get("sub") is None
+    assert decoded_token.get("aud") is None
+    assert decoded_token.get("nbf") is None
+
+
+def test_jwt():
+    encoded_token = _encode_jwt(
+        extra_claims={"username": "bamboo"},
+        secret_key="bamboo",
+        issuer="issuer",
+        subject="subject",
+        audience="audience",
+        nbf=True,
+        jti=True,
+    )
+
+    decoded_token = _decode_jwt(
+        encoded_token=encoded_token, secret_key="bamboo", audience="audience"
+    )
+
+    assert decoded_token["username"] == "bamboo"
+    assert decoded_token["type"] == "access"
+    assert decoded_token["iat"] is not None
+    assert decoded_token["exp"] == (decoded_token["iat"] + timedelta(minutes=30).total_seconds())
+    assert decoded_token["iss"] == "issuer"
+    assert decoded_token["sub"] == "subject"
+    assert decoded_token["aud"] == "audience"
+    assert decoded_token["nbf"] == decoded_token["iat"]
+    assert decoded_token["jti"] is not None
+
+    with pytest.raises(JWTClaimsError, match="Invalid audience"):
+        _decode_jwt(
+            encoded_token=encoded_token,
+            secret_key="bamboo",
+        )
+
+
+def test_expired_jwt():
+    encoded_token = _encode_jwt(
+        extra_claims={"username": "bamboo"},
+        secret_key="bamboo",
+        expires_delta=timedelta(seconds=-1),
+    )
+
+    with pytest.raises(ExpiredSignatureError):
+        _decode_jwt(encoded_token=encoded_token, secret_key="bamboo")
+
+
+def test_invalid_jwt():
+    encoded_token = _encode_jwt(
+        extra_claims={"username": "bamboo"},
+        secret_key="bamboo",
+    )
+
+    with pytest.raises(JWTError):
+        _decode_jwt(encoded_token=encoded_token + "wrong suffix", secret_key="bamboo")

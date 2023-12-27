4d20ac55ae2ce58db601e439915de2dbb3a5ea42
Frank Yu <withyuxiaoy@gmail.com>
2023-12-27 11:36:10 +0800

    feat: add error blueprint (#19)
    feature: add error blueprint

--- backend/bamboo/blueprints/__init__.py
+++ backend/bamboo/blueprints/__init__.py
@@ -4,12 +4,14 @@ from bamboo.blueprints.admin import admin
 from bamboo.blueprints.auth import auth
 from bamboo.blueprints.blog import blog
 from bamboo.blueprints.command import command
+from bamboo.blueprints.error import error
 from bamboo.blueprints.page import page
 from bamboo.blueprints.talk import talk
 
 
 def init_app(app: APIFlask) -> None:
     app.register_blueprint(command)
+    app.register_blueprint(error)
     app.register_blueprint(admin, url_prefix="/admin")
     app.register_blueprint(auth, url_prefix="/auth")
     app.register_blueprint(blog, url_prefix="/blog")

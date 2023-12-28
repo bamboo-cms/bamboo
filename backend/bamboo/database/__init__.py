21c477aa86e2ceced2a76125f3a0487d09c68521
Frost Ming <me@frostming.com>
2023-12-28 11:21:49 +0800

    feat: improve dockerfile and docker-compose
    Signed-off-by: Frost Ming <me@frostming.com>

--- backend/bamboo/database/__init__.py
+++ backend/bamboo/database/__init__.py
@@ -1,9 +1,11 @@
+from pathlib import Path
+
 from flask import Flask
 from flask_migrate import Migrate
 
 from bamboo.database.models import db
 
-migrate = Migrate(db=db)
+migrate = Migrate(db=db, directory=Path(__file__).with_name("migrations"))
 
 
 def init_app(app: Flask) -> None:

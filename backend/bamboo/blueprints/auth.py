3c77ae44678840794d224f7065d579347895c3be
Frost Ming <me@frostming.com>
2024-01-16 19:29:28 +0800

    fix: improve auth handling
    Signed-off-by: Frost Ming <me@frostming.com>

--- backend/bamboo/blueprints/auth.py
+++ backend/bamboo/blueprints/auth.py
@@ -1,18 +1,17 @@
 from datetime import timedelta
-from typing import Any, Callable
+from typing import Callable, TypeVar, overload
 
-from apiflask import (
-    APIBlueprint,
-    HTTPTokenAuth,
-    abort,
-)
+from apiflask import APIBlueprint, HTTPTokenAuth, abort
 from flask import current_app
 from flask_httpauth import Authorization
 from jose.exceptions import JWTError
+from traitlets import Any
 
 from bamboo.database import db, models
 from bamboo.schemas.auth import LoginSchema, TokenSchema
 from bamboo.utils import decode_jwt, encode_jwt
+
+F = TypeVar("F", bound=Callable)
 
 MANAGE_SITE = 0b001
 MANAGE_USER = 0b010
@@ -21,12 +20,41 @@ PERMISSIONS = (MANAGE_SITE, MANAGE_USER, MANAGE_CONTENT)
 
 
 class TokenAuth(HTTPTokenAuth):
-    def auth_required(
-        self, f: Callable | None = None, permissions: int = 0, optional: Any | None = None
-    ) -> Any:
-        """A wrapper of `login_required`.
+    def authorize(
+        self, permissions: int | None, user: models.User, _: Authorization | None
+    ) -> bool:
+        """Overriding authorize() to improve efficiency."""
+        if permissions is None:
+            return True
 
-            Examples:
+        if self.get_user_roles_callback is None:
+            raise ValueError("get_user_roles callback is not defined")
+
+        user_permissions = self.ensure_sync(self.get_user_roles_callback)(user)
+
+        return permissions & user_permissions == permissions
+
+    @property
+    def current_user(self) -> models.User | None:
+        """Overriding current_user to offer type information."""
+        return super().current_user
+
+    @overload
+    def auth_required(self, f: F) -> F:
+        ...
+
+    @overload
+    def auth_required(
+        self, *, permissions: int | None = None, optional: Any = None
+    ) -> Callable[[F], F]:
+        ...
+
+    def auth_required(
+        self, f: F | None = None, *, permissions: int | None = None, optional: Any = None
+    ) -> F | Callable[[F], F]:
+        """A wrapper of login_required() supporting bitwise permission check.
+
+        Examples:
             Only login is required.
             ```python
             from bamboo.blueprints.auth import token_auth, MANAGE_USER
@@ -56,25 +84,9 @@ class TokenAuth(HTTPTokenAuth):
             @token_auth.auth_required(permissions=MANAGE_SITE | MANAGE_USER)
             def manage_site_and_user():
                 ...
-        `    ``
+            ```
         """
-        if f:
-            return self.login_required(f=f)
-        return self.login_required(f=None, role=permissions, optional=optional)
-
-    def authorize(
-        self, permissions: int | None, user: dict[str, Any] | None, _: Authorization | None
-    ) -> bool:
-        """Overriding authorize() to improve efficiency."""
-        if permissions is None:
-            return True
-
-        if self.get_user_roles_callback is None:
-            raise ValueError("get_user_roles callback is not defined")
-
-        user_permissions = self.ensure_sync(self.get_user_roles_callback)(user)
-
-        return (permissions & user_permissions) == permissions
+        return self.login_required(f, role=permissions, optional=optional)
 
 
 token_auth = TokenAuth()
@@ -113,13 +125,9 @@ def login(json_data):
 
 @auth.post("/refresh")
 @auth.output(TokenSchema)
-@token_auth.auth_required
+@token_auth.login_required
 def refresh():
-    user_id = token_auth.current_user.get("user_id")
-    user = db.session.get(models.User, user_id)
-
-    if user is None:
-        abort(401)
+    user = token_auth.current_user
 
     if user.role is None or user.role.permissions == 0:
         abort(403)
@@ -133,22 +141,19 @@ def refresh():
 
 
 @token_auth.verify_token
-def verify_token(token: str) -> dict[str, Any] | None:
+def verify_token(token: str) -> models.User:
     try:
         payload = decode_jwt(encoded_token=token, secret_key=current_app.config.get("SECRET_KEY"))
     except JWTError as error:
         abort(401, str(error))
-
-    return {"user_id": payload.get("user_id")}
+    user = db.session.get(models.User, payload.get("user_id"))
+    if user is None:
+        abort(401)
+    return user
 
 
 @token_auth.get_user_roles
-def get_user_permissions(payload: dict[str, Any]) -> int:
-    user_id = payload.get("user_id")
-    user = db.session.get(models.User, user_id)
-    if user is None:
-        abort(401)
-
+def get_user_permissions(user: models.User) -> int:
     if user.role is None:
         abort(403)
 
